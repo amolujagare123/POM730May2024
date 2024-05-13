3fce90460ec0f1b601019f2b2f1b98204849868c
amolu <amolujagare@gmail.com>
2024-05-13 20:33:34 +0530

    Initial commit

---
+++ src/test/java/regression/LoginTest.java
@@ -0,0 +1,31 @@
+package regression;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.FindBy;
+import org.testng.annotations.Test;
+import pages.Login;
+
+public class LoginTest {
+
+  @Test
+  public void loginTest()
+  {
+    WebDriver driver = new ChromeDriver();
+    driver.manage().window().maximize();
+    driver.get("http://localhost/ip");
+
+    Login login = new Login(driver);
+        
+    login.setTxtUsername("amolujagare@gmail.com");
+    login.setTxtPassword("admin123");
+    login.clickLogin();
+
+  }
+
+
+
+
+}
